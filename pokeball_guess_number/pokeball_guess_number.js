3186437f97d26dcb673168ad451cce81596385bc
Floed1 <daniel_fessing@gmx.de>
2025-03-14 11:44:38 +0100

    changes

--- pokeball_guess_number/pokeball_guess_number.js
+++ pokeball_guess_number/pokeball_guess_number.js
@@ -1,23 +1,37 @@
 let randomNumber;
 let upperLimit = 100;
 let guessedNumber;
-let tries;
+let tries = 0;
 
 function startGuessNumbers() {
 
     randomNumber = Math.random() * upperLimit;
     randomNumber = Math.round(randomNumber + 0.5);
 
-    document.getElementById("game-text2").innerText = "Rate eine Zahl zwischen 0 und 100";
+    document.getElementById("game-text2").innerText = "Rate eine Zahl zwischen 0 und 100, klicke dafür in die Mitte";
 }
 
 function checkInput() {
-    if (randomNumber == null); {
+    if (randomNumber == null) {
         return;
-        }
+    }
 
-        guessedNumber = document.getElementById("input").ariaValue;
-        document.getElementById("game-text1").innerText = guessedNumber;
+    guessedNumber = document.getElementById("input").value
+    document.getElementById("game-text1").innerText = guessedNumber;
 
-        return false;
+    if (randomNumber > guessedNumber) {
+        document.getElementById("game-text2").innerText = "Deine Zahl ist zu klein";
+    }
+    
+    else if (randomNumber < guessedNumber) {
+        document.getElementById("game-text2").innerText = "Deine Zahl ist zu groß";
+    }
+
+    else if (randomNumber == guessedNumber) {
+        document.getElementById("game-text2").innerText = "Richtig geraten." + " Du hast " + tries + " Versuche gebraucht.";
+    }
+
+    tries = tries + 1;
+
+    return false;
 }
